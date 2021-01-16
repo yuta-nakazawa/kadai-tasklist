247bf4812fb883767bec64eafcc8c6322bf85299
jiro.kirameki <taro.kirameki@techacademy.jp>
2021-01-16 19:42:54 +0900

    kadai-tasklist

---
+++ WebContent/WEB-INF/views/app.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>tasks</h1>
+            </div>
+            <div id="content">
+                ${param.content}
+            </div>
+        </div>
+    </body>
+</html>
---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+
+        em.close();
+
+        request.setAttribute("messages", messages);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        rd.forward(request, response);
+    }
+
+}
